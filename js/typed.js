d76442db9178e8000e53fbc514e9c3fc0eac1a90
William <william.2025@hotmail.com>
2019-06-24 12:35:35 -0400

    Update!

---
+++ js/typed.js
@@ -0,0 +1,16 @@
+let aboutStrings = [
+    "software engineer",
+    "lifelong learner",
+    "problem solver",
+    "Machine Learning Enthusiast"
+  ];
+  
+  let options = {
+    strings: aboutStrings,
+    typeSpeed: 100,
+    loop: true,
+    backDelay: 1200
+  };
+  
+let typed = new Typed("#text", options);
+  
