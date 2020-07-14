fc7c87dfa057de7b9a1c0eeeb8f17b3eb20cedc7
duarte2025 <william.2025@hotmail.com>
2020-07-13 20:24:37 -0400

    refactor: changes in css and text

--- js/typed.js
+++ js/typed.js
@@ -1,16 +1,15 @@
 let aboutStrings = [
-    "software engineer",
-    "lifelong learner",
-    "problem solver",
-    "Machine Learning Enthusiast"
-  ];
-  
-  let options = {
-    strings: aboutStrings,
-    typeSpeed: 100,
-    loop: true,
-    backDelay: 1200
-  };
-  
+  "software engineer",
+  "lifelong learner",
+  "problem solver",
+  "Scientist Enthusiast"
+];
+
+let options = {
+  strings: aboutStrings,
+  typeSpeed: 100,
+  loop: true,
+  backDelay: 1200
+};
+
 let typed = new Typed("#text", options);
-  
